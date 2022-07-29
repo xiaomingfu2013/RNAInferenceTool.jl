e0288e6a44e847507a78f3fc9d12597ab6b07155
xiaoming <palmtree2013@163.com>
2022-07-29 08:52:05 +0800

    add

--- examples/sythetic_data.jl
+++ examples/sythetic_data.jl
@@ -14,10 +14,36 @@ L1 = 862; L = 3062; # L1 =  signal fluorescence 862 bp; L = total gene 3062 bp
 numofRun = 10000 # delay SSA sample size
 djprob = construct_prob_delaytelegraph(params)
 ens_prob = EnsembleProblem(djprob)
-ens = solve(ens_prob, SSAStepper(), EnsembleThreads(), trajectories = numofRun)
+# solve(djprob, SSAStepper())
+ens = solve(ens_prob, SSAStepper(), EnsembleSerial(), trajectories = numofRun)
 
 
 filter_pos_tf = [ens[i].channel[end][1] for i in eachindex(ens)]
 res_ens = [signal_function.(filter_pos_tf[i], τ = τ, L1 = L1, L = L) for i in eachindex(filter_pos_tf)]
 histo_synthetic = sum_with_non.(res_ens)
 scatter(convert_histo(histo_synthetic), labels="synthetic data") # plot distribution
+
+
+# Inference
+
+#For delay model the parameter order: σ_off, σ_on, ρ_on, ρ_off, d, τ
+SRange = [(0.0,50.0),(0.0,50.0),(0.0,100.0),(0.0,0.0),(0.0,0.0),(τ,τ)];
+
+#For telegraph model the parameter order: σ_off, σ_on, ρ_on, ρ_off, d
+SRange_tele = [(0.0,50.0),(0.0,50.0),(0.0,100.0),(0.0,0.0),(1/τ,1/τ)];
+
+infer_struct = OptimStruct(histo_synthetic, G1(), Likelihood(), Delay(); infer_counts = false, L1 = 862, L2 =2200)
+infer_struct_tele = OptimStruct(histo_synthetic, G1(), Likelihood(), Telegraph(); infer_counts = false, L1 = 862, L2 =2200)
+
+estimated_params, distributions = optim_function(SRange, infer_struct, MaxFuncEvals = 10000)
+
+
+estimated_params_tele, distributions_tele = optim_function(SRange_tele, infer_struct_tele, MaxFuncEvals = 10000)
+
+
+scatter(distributions[:,2],labels="synthetic data")
+plot!([distributions[:,1] distributions_tele[:,1]],lines=(2, :dash),labels=["Delay Telegraph" "Telegraph"])
+
+
+#Compare the paramters
+DataFrame(True = params[1:3],Delay=estimated_params[1:3],Telegraph= estimated_params_tele[1:3])
